97b6f09f99bf5c414c804951acc5ebab67d1aaf0
samuelcox <sam.r.c@hotmail.com>
2021-02-27 20:50:03 +0000

    add .net 5.0 tests

---
+++ csharp-dns/Program.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Net;
+using System.Threading;
+
+Dns.GetHostAddressesAsync("http://localhost:8080");
+
+for (int i = 0; i < 25; i++)
+{
+    Dns.GetHostAddressesAsync("http://example.org");
+}
+
+Thread.Sleep(1000);
+
+Dns.GetHostAddressesAsync("http://localhost:8080");
+
